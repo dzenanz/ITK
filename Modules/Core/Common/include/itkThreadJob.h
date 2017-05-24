83daf2536cb9feff3df4a05da32ca75922c609e4
Dženan Zukić <dzenan.zukic@kitware.com>
2017-05-24 14:48:57 -0400

    DOC: updating documentation of ThreadJob
    Change-Id: I6137dd0701c8c079687ef04fb79312e6a9e0440c

--- Modules/Core/Common/include/itkThreadJob.h
+++ Modules/Core/Common/include/itkThreadJob.h
@@ -27,12 +27,10 @@ namespace itk
  * \class ThreadJob
  *
  * \brief This class is used to submit jobs to the thread pool.
- * The thread job maintains important information of the submitted job
- * such as Job Id, information to identify if the job has finished executing.
- * It holds the function pointer that the user sets to the function the
- * user wants to be executed in parallel by the thread pool.
- * Also holds the args pointer - it is passed to the executing function by
- * the thread pool.
+ *
+ * The thread job contains information of the submitted job:
+ * Job Id, the function to be executed in parallel and its argument.
+ *
  * \ingroup OSSystemObjects
  * \ingroup ITKCommon
  */
@@ -53,18 +51,17 @@ public:
   }
 
 
-/** Declaring function that will be called */
+/** Function that will be called. */
 #if defined(_WIN32) || defined(_WIN64)
     DWORD ( __stdcall *m_ThreadFunction )( void * ptr );
 #else
     void * (*m_ThreadFunction)(void *ptr);
 #endif
 
-  /** This is the Job's id. If it is -1 it means the job hasn't been
-    initialized*/
+  /** This is the Job's id. Used for waiting on this job's completion. */
   JobIdType m_Id;
 
- /** Stores the user's data that needs to be passed into the function */
+ /** Stores the user's data that needs to be passed into the function. */
   void *m_UserData;
 };
 
