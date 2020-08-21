43ee4a71553f16c7bc54fef00f35f3e5eb5eae89
Dženan Zukić <dzenan.zukic@kitware.com>
2020-08-21 10:35:38 -0400

    WIP: testing

--- Modules/ThirdParty/PNG/UpdateFromUpstream.sh
+++ Modules/ThirdParty/PNG/UpdateFromUpstream.sh
@@ -2,8 +2,8 @@
 
 thirdparty_module_name='PNG'
 
-upstream_git_url='git://git.code.sf.net/p/libpng/code'
-upstream_git_branch='libpng16'
+upstream_git_url='git://github.com/dzenanz/libpng'
+upstream_git_branch='master'
 
 snapshot_author_name='LIBPNG Upstream'
 snapshot_author_email='png-mng-implement@lists.sourceforge.net'
